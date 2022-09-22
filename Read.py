2c371ff0ac309bcfc459f865f59eafd22a15ae38
Dino Yang <s1136300@student.hsleiden.nl>
2022-09-23 01:10:06 +0200

    commit

---
+++ Read.py
@@ -0,0 +1,30 @@
+import csv
+import random
+from time import time
+from time import sleep as wait
+
+conn = None
+try:
+    conn = #connect to database
+    cur = conn.cursor()
+    with open('#csv_name', 'w', newline='') as f1:
+        writer = csv.writer(f1)
+        writer.writerow(["x", "exec_time"])
+        for x in range(0, 10000):
+            n = random.randint(1, 100000)
+            query = f"SELECT dataset.id, dataset.pattern, dummyset.achternaam FROM dataset INNER JOIN dummyset ON dataset.id = dummyset.id WHERE dataset.id = {n}"
+            tic = time()
+            cur.execute(query)
+            toc = time()
+            execTime = toc - tic
+            result = cur.fetchone()
+            data = [x, execTime * 1000]
+            writer.writerow(data)
+        print("Done!")
+    cur.close()
+except (Exception) as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
---
+++ Write.py
@@ -0,0 +1,31 @@
+import csv
+import random
+from time import time
+from time import sleep as wait
+
+
+
+try:
+    conn = #connect to database
+    cur = conn.cursor()
+    with open('#csv_name', 'w', newline='') as f1:
+        writer = csv.writer(f1)
+        writer.writerow(["x", "exec_time"])
+        for x in range(0, 10000):
+            query1 = f"insert into dataset (pattern,print,fabric,native_product_id,primary_colour) values ('a','b','b',{x},'b')"
+            query2 = "DELETE FROM dataset  WHERE ID=(SELECT MAX(id) FROM dataset)"
+            tic = time()
+            cur.execute(query1)
+            toc = time()
+            cur.execute(query2)
+            execTime = toc - tic
+            data = [x, execTime * 1000]
+            writer.writerow(data)
+        print("Done!")
+    cur.close()
+except Exception as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
